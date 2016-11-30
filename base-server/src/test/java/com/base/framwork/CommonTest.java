9a4cde4ecdec1ec5f4a14bb1ddb9b3b6d5dfe364
周艺伟 <ywzhou@yilepay.com>
2016-11-30 18:28:17 +0800

    test mybatis updateBy deleteBy
    zookeeper dubbo

--- base-server/src/test/java/com/base/framwork/CommonTest.java
+++ base-server/src/test/java/com/base/framwork/CommonTest.java
@@ -12,9 +12,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
  * @date 2016.09.04
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {"classpath:spring.xml"})
+@ContextConfiguration(locations = {"classpath:/META-INF/spring/*.xml"})
 public class CommonTest {
-
     protected Logger logger = LoggerFactory.getLogger(getClass());
-
 }
