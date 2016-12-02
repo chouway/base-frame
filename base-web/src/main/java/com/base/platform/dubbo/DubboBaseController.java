6826c2126bc8d870a565a08753505ef013df2c1c
周艺伟 <ywzhou@yilepay.com>
2016-12-02 16:45:10 +0800

    spring mvc velocity

--- base-web/src/main/java/com/base/platform/dubbo/DubboBaseController.java
+++ base-web/src/main/java/com/base/platform/dubbo/DubboBaseController.java
@@ -1,10 +1,18 @@
 package com.base.platform.dubbo;
 
+import com.alibaba.fastjson.JSON;
 import com.base.platform.dubbo.service.IBaseServerService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * BaseController
@@ -15,11 +23,38 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/dubbo/base")
 public class DubboBaseController {
 
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
+
     @Autowired
     private IBaseServerService baseServerService;
 
     @RequestMapping(value = "/server.htm")
     public String init(Model model){
+
+        model.addAttribute("method","method");
+
+        Map<String,Object> tempMap = new HashMap<String,Object>();
+        tempMap.put("map_field_key","map_field_val");
+        model.addAllAttributes(tempMap);
+
+        List<String> list = new ArrayList<String>();
+        list.add("item_0");
+        list.add("item_1");
+        list.add("item_2");
+
+
+        Map<String,Object> map = new HashMap<String,Object>();
+        map.put("key_0","val_0");
+        map.put("key_1","val_1");
+        map.put("key_2","val_2");
+
+
+        model.addAttribute("map",map);
+        model.addAttribute("list",list);
+
+        Map<String, Object> modelMap = model.asMap();
+        logger.info("-->modelMap={}", JSON.toJSONString(modelMap));
         return "/dubbo/base/server.html";
     }
 }
