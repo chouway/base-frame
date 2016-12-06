ca07e403ce6d990af8906e4274233b424e75dba9
周艺伟 <ywzhou@yilepay.com>
2016-12-06 18:10:40 +0800

    simple active mq todo

---
+++ base-simple/src/test/java/com/base.simple/activemq/MessageReciever.java
@@ -0,0 +1,96 @@
+package com.base.simple.activemq;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.Message;
+import javax.jms.MessageConsumer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import org.apache.activemq.ActiveMQConnection;
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+/**
+ * <b>function:</b> 消息接收者
+ * @author hoojo
+ * @createDate 2013-6-19 下午01:34:27
+ * @file MessageReceiver.java
+ * @package com.hoo.mq.jms
+ * @project ActiveMQ-5.8
+ * @blog http://blog.csdn.net/IBM_hoojo
+ * @email hoojo_@126.com
+ * @version 1.0
+ */
+public class MessageReciever {
+
+    // tcp 地址
+    public static final String BROKER_URL = "tcp://localhost:61616";
+
+    // 目标，在ActiveMQ管理员控制台创建 http://localhost:8161/admin/queues.jsp
+    public static final String DESTINATION = "com.base.simple.activemq.p2p.msg";
+
+    //最大接收消息数
+    public static final int MAX_RECIEVE_NUM = 25;
+
+    // 接收数据的时间（等待） ms
+    public static final long LISTEN_TIME = 30 * 1000l;
+
+
+
+    public static void run(String id) throws Exception {
+
+        Connection connection = null;
+        Session session = null;
+        try {
+            // 创建链接工厂
+            ConnectionFactory factory = new ActiveMQConnectionFactory(ActiveMQConnection.DEFAULT_USER, ActiveMQConnection.DEFAULT_PASSWORD, BROKER_URL);
+            // 通过工厂创建一个连接
+            connection = factory.createConnection();
+            // 启动连接
+            connection.start();
+            // 创建一个session会话
+            session = connection.createSession(Boolean.TRUE, Session.AUTO_ACKNOWLEDGE);
+            // 创建一个消息队列
+            Destination destination = session.createQueue(DESTINATION);
+
+            // 创建消息消费者
+            MessageConsumer consumer = session.createConsumer(destination);
+
+            int count = 0;
+            while (true) {
+                // 接收数据的时间（等待） 100 s
+                Message message = consumer.receive(LISTEN_TIME);
+
+                Thread.sleep(250l);
+                TextMessage text = (TextMessage) message;
+                if (text != null) {
+                    System.out.println(id + "-接收-" + count +"：" + text.getText());
+                    ++count;
+                    if(count<MAX_RECIEVE_NUM){
+                        continue;
+                   }
+                }
+                // 提交会话  // message dequeued   p2p
+                System.out.println(id + "-MessageReceiver break:count=" + count);
+                consumer.close();
+                session.commit();
+                break;
+            }
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            // 关闭释放资源
+            if (session != null) {
+                session.close();
+            }
+            if (connection != null) {
+                connection.close();
+            }
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        MessageReciever.run("id_0");
+        MessageReciever.run("id_1");
+    }
+}
---
+++ base-simple/src/test/java/com/base.simple/activemq/QueueReciever.java
@@ -0,0 +1,91 @@
+package com.base.simple.activemq;
+import javax.jms.JMSException;
+import javax.jms.MapMessage;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.Queue;
+import javax.jms.QueueConnection;
+import javax.jms.QueueConnectionFactory;
+import javax.jms.QueueSession;
+import javax.jms.Session;
+import org.apache.activemq.ActiveMQConnection;
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+import java.util.Properties;
+
+/**
+ * <b>function:</b> 消息接收者； 依赖hawtbuf-1.9.jar
+ * @author hoojo
+ * @createDate 2013-6-19 下午01:34:27
+ * @file MessageReceiver.java
+ * @package com.hoo.mq.queue
+ * @project ActiveMQ-5.8
+ * @blog http://blog.csdn.net/IBM_hoojo
+ * @email hoojo_@126.com
+ * @version 1.0
+ */
+public class QueueReciever {
+
+    // tcp 地址
+    public static final String BROKER_URL = "tcp://localhost:61616";
+    // 目标，在ActiveMQ管理员控制台创建 http://localhost:8161/admin/queues.jsp
+    public static final String TARGET = "com.base.simple.activemq.p2p.jms";
+
+
+    public static final long SLEEP_TIME = 60 * 1000l;
+
+    public static void run(final String id) throws Exception {
+
+        QueueConnection connection = null;
+        QueueSession session = null;
+        try {
+            // 创建链接工厂
+            QueueConnectionFactory factory = new ActiveMQConnectionFactory(ActiveMQConnection.DEFAULT_USER, ActiveMQConnection.DEFAULT_PASSWORD, BROKER_URL);
+
+            // 通过工厂创建一个连接
+            connection = factory.createQueueConnection();
+            // 启动连接
+            connection.start();
+            // 创建一个session会话
+            session = connection.createQueueSession(Boolean.TRUE, Session.AUTO_ACKNOWLEDGE);
+            // 创建一个消息队列
+            Queue queue = session.createQueue(TARGET);
+            // 创建消息接收者
+            javax.jms.QueueReceiver receiver = session.createReceiver(queue);
+
+
+            receiver.setMessageListener(new MessageListener() {
+                public void onMessage(Message msg) {
+                    if (msg != null) {
+                        MapMessage map = (MapMessage) msg;
+                        try {
+                            System.out.println(id + "-" + map.getLong("time") + "接收#" + map.getString("text"));
+                        } catch (JMSException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            });
+            // 休眠再关闭
+            Thread.sleep(SLEEP_TIME);
+            System.out.println(id + "-queueReceive commit" );
+            // 提交会话
+            session.commit();
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            // 关闭释放资源
+            if (session != null) {
+                session.close();
+            }
+            if (connection != null) {
+                connection.close();
+            }
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        QueueReciever.run("id_0");
+        QueueReciever.run("id_1");
+    }
+}
